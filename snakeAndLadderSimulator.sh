9080391cb34dde8e6761c8227df5da6917b3998f
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-30 15:39:04 +0530

    ADDED: function for rolling a dice

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -3,3 +3,9 @@ echo "WELCOME TO SNAKE AND LADDER SIMULATOR"
 START_POSITION=0
 #variable
 playerOnePosition=$START_POSITION
+
+function rollingDice () {
+	numberOnDice=$(($((RANDOM%6))+1))
+	echo "Number after rolling the dice : $numberOnDice"
+}
+rollingDice
