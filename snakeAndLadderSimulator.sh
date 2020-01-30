aded86c3acc4585ad1983f6be53f1f3bd7a3d8b6
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-30 17:55:01 +0530

    ADDED:function for reaching till winning position

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -5,6 +5,7 @@ START_POSITION=0
 NO_PLAY=0
 LADDER=1
 SNAKE=2
+WINNING_POSITION=100
 #variable
 playerOnePosition=$START_POSITION
 
@@ -24,7 +25,17 @@ function checkForOptionsNoplayLadderOrSnake () {
 	elif [ $option -eq $SNAKE ]
 	then
 		playerOnePosition=$(($playerOnePosition - $numberOnDice))
+		if [ $playerOnePosition -lt $START_POSITION ]
+		then
+			playerOnePosition=$START_POSITION
+		fi
 	fi
 	echo "Player is at position : $playerOnePosition"
 }
-rollingDice
+function repeatingTillWinningPosition () {
+	while [[ $playerOnePosition -lt $WINNING_POSITION ]]
+	do
+		rollingDice
+	done
+}
+repeatingTillWinningPosition
