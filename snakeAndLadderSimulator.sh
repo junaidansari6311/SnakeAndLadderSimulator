626440354c32da2842ecc276c1df256b771126eb
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-01 16:53:12 +0530

    ADDED:function for switching the turns of players and checking winner

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -x
 echo "WELCOME TO SNAKE AND LADDER SIMULATOR"
 #constants
 START_POSITION=0
@@ -8,40 +8,67 @@ SNAKE=2
 WINNING_POSITION=100
 #variable
 playerOnePosition=$START_POSITION
-diceCount=0
+playerTwoPosition=$START_POSITION
+currentPlayerPosition=$START_POSITION
+diceCountOfPlayerOne=0
+diceCountOfPlayerTwo=0
+flag=0
 function rollingDice () {
 	numberOnDice=$(($((RANDOM%6))+1))
-	echo "Number after rolling the dice : $numberOnDice"
 	checkForOptionsNoplayLadderOrSnake
-	((diceCount++))
 }
 function checkForOptionsNoplayLadderOrSnake () {
 	option=$((RANDOM%3))
 	if [ $option -eq $NO_PLAY ]
 	then
-		playerOnePosition=$playerOnePosition
+		currentPlayerPosition=$currentPlayerPosition
 	elif [ $option -eq $LADDER ]
 	then
-		playerOnePosition=$(($playerOnePosition + $numberOnDice))
-		if [ $playerOnePosition -gt $WINNING_POSITION ]
+		currentPlayerPosition=$(($currentPlayerPosition + $numberOnDice))
+		if [ $currentPlayerPosition -gt $WINNING_POSITION ]
 		then
-			playerOnePosition=$(($playerOnePosition - $numberOnDice))
+			currentPlayerPosition=$(($currentPlayerPosition - $numberOnDice))
 		fi
 	elif [ $option -eq $SNAKE ]
 	then
-		playerOnePosition=$(($playerOnePosition - $numberOnDice))
-		if [ $playerOnePosition -lt $START_POSITION ]
+		currentPlayerPosition=$(($currentPlayerPosition - $numberOnDice))
+		if [ $currentPlayerPosition -lt $START_POSITION ]
 		then
-			playerOnePosition=$START_POSITION
+			currentPlayerPosition=$START_POSITION
 		fi
 	fi
-	echo "Player is at position : $playerOnePosition"
+}
+function switchTurnsOfPlayers () {
+	if [[ flag -eq 0 ]]
+	then
+		currentPlayerPosition=$playerOnePosition
+		rollingDice
+		playerOnePosition=$currentPlayerPosition
+		((diceCountOfPlayerOne++))
+		flag=1
+	else
+		currentPlayerPosition=$playerTwoPosition
+		rollingDice
+		playerTwoPosition=$currentPlayerPosition
+		((diceCountOfPlayerTwo++))
+		flag=0
+	fi
 }
 function repeatingTillWinningPosition () {
-	while [[ $playerOnePosition -lt $WINNING_POSITION ]]
+	while [[ $currentPlayerPosition -lt $WINNING_POSITION ]]
 	do
-		rollingDice
+		switchTurnsOfPlayers
 	done
 }
+function checkWinner () {
+	if [ $playerOnePosition -eq $WINNING_POSITION ]
+	then
+		echo "Player one won"
+	else
+		echo "Player two won"
+	fi
+}
 repeatingTillWinningPosition
-echo "Number of Time Dice Tossed for Winning: $diceCount"
+checkWinner
+echo "Number of times dice tossed by player one: $diceCountOfPlayerOne"
+echo "Number of times dice tossed by player two: $diceCountOfPlayerTwo"
